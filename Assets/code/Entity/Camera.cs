85f0eb7fd4b5032c3d024ad068566994cfc90b98
LeoPrattThomas <lpratt-thomas@ucls.uchicago.edu>
2023-03-20 09:22:03 -0500

    camra follow 2

--- Assets/code/Entity/Camera.cs
+++ Assets/code/Entity/Camera.cs
@@ -2,33 +2,15 @@ using UnityEngine;
 using System.Collections;
 
 public class Camera : MonoBehaviour {
+    public Transform player;
+    public Vector3 offset;
 
-  public GameObject Player;
-  private Vector3 _endPoint;
-
-  private const float DISTANCE_THRESHOLD = 6.0f;
-
-  void Start () {
-    if (this.Player != null) {
-      this._endPoint = this.Player.transform.position;
+    void Update () 
+    {
+        // Camera follows the player with specified offset position
+        transform.position = new Vector3 (
+            player.position.x + offset.x, 
+            player.position.y + offset.y, 
+            offset.z);
     }
-  }
-
-  void Update () {
-    if (this.Player != null) {
-      // Check if the distance between the player and the ball is big enough to start moving
-      if (Vector3.Distance(this.Player.transform.position, this.transform.position) > DISTANCE_THRESHOLD) {
-        // Start moving towards the player
-        this.transform.position = Vector3.Slerp(this.transform.position, this._endPoint, Time.deltaTime);
-      }
-      // Check if the tracker has finally reached its end point. If it hasn't, keep on moving.
-      if (this.transform.position != this._endPoint) {
-        this.transform.position = Vector3.Slerp(this.transform.position, this._endPoint, Time.deltaTime);
-      }
-      
-
-      // Update the last frame's player position
-      this._endPoint = this.Player.transform.position;
-    }
-  }
 }
